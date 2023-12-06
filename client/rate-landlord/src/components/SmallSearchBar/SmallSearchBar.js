3283718dd8a3e515b8c4de266c4318d8e8ff5b9b
Sheida Rashidi <84582226+SheidaRa@users.noreply.github.com>
2023-12-06 14:08:17 +0100

    Add files via upload

---
+++ client/rate-landlord/src/components/SmallSearchBar/SmallSearchBar.js
@@ -0,0 +1,95 @@
+import React, { useState } from "react";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import Divider from "@material-ui/core/Divider";
+import { FaSearch } from "react-icons/fa";
+import "./SmallSearchBar.css";
+
+const NOMINATIM_BASE_URL = "https://nominatim.openstreetmap.org/search?";
+const params = {
+  q: "",
+  format: "json",
+  addressdetails: "addressdetails",
+};
+
+export default function SearchBox(props) {
+  const { selectPosition, setSelectPosition } = props;
+  const [searchText, setSearchText] = useState("");
+  const [listPlace, setListPlace] = useState([]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  }
+
+  return (
+    
+    <>
+    <form className="form-row" onSubmit={handleSubmit}> {/* Use Bootstrap's input group */}
+    <div className="small-search-bar">
+      <input
+        type="text"
+        placeholder="Enter an address, neighborhood, city, or ZIP code"
+        value={searchText}
+        onChange={(event) => {
+          setSearchText(event.target.value);
+        }}
+      />
+      <button
+      type="submit"
+      onClick={() => {
+        // Search
+        const params = {
+          q: searchText,
+          format: "json",
+          addressdetails: 1,
+          polygon_geojson: 0,
+        };
+        const queryString = new URLSearchParams(params).toString();
+        const requestOptions = {
+          method: "GET",
+          redirect: "follow",
+        };
+        fetch(`${NOMINATIM_BASE_URL}${queryString}`, requestOptions)
+          .then((response) => response.text())
+          .then((result) => {
+            console.log(JSON.parse(result));
+            setListPlace(JSON.parse(result));
+          })
+          .catch((err) => console.log("err: ", err));
+      }}
+      >
+        <FaSearch />
+      </button>
+    </div>
+  </form>
+  <List component="nav" aria-label="main mailbox folders">
+          {listPlace.map((item) => {
+            return (
+              <div key={item?.place_id}>
+                <ListItem
+                  button
+                  onClick={() => {
+                    setSelectPosition(item);
+                    setListPlace([]);
+                  }}
+                >
+                  <ListItemIcon>
+                    <img
+                      src="/pin.svg"
+                      alt="Placeholder"
+                      style={{ width: 38, height: 38 }}
+                    />
+                  </ListItemIcon>
+                  <ListItemText primary={item?.display_name} />
+                </ListItem>
+                <Divider />
+              </div>
+            );
+          })}
+        </List>
+  </>
+
+  );
+}
