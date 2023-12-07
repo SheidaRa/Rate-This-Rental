1dc9088ca4c6f6786669a9eb4db9ad034cd07503
Sheida Rashidi <84582226+SheidaRa@users.noreply.github.com>
2023-12-07 13:20:30 +0100

    Add files via upload

--- client/rate-landlord/src/components/Map/Map.js
+++ client/rate-landlord/src/components/Map/Map.js
@@ -1,74 +1,27 @@
-import React, { useEffect } from "react";
-import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
-import L from "leaflet";
-import "./Map.css";
+import Map from '../components/Map/Map';
+import { useState } from "react";
+import SmallSearchBar from '../components/SmallSearchBar/SmallSearchBar'
 
-const icon = L.icon({
-  iconUrl: "/pin.svg",
-  iconSize: [38, 38], 
-});
 
-const position = [44.95, -93.09];
+import "../components/Searchbar/SearchBar.css"
+import "../components/Searchbar/SearchResultsList.css"
+import "../components/Searchbar/SearchResult.css"
 
-function ResetCenterView(props) {
-  const { selectPosition } = props;
-  const map = useMap();
 
-  useEffect(() => {
-    if (selectPosition) {
-      map.setView(
-        L.latLng(selectPosition?.lat, selectPosition?.lon),
-        map.getZoom(),
-        {
-          animate: true
-        }
-      )
-    }
-  }, [selectPosition]);
-
-  return null;
-}
-
-export default function Maps(props) {
-
-  const { selectPosition } = props;
-  const locationSelection = [selectPosition?.lat, selectPosition?.lon];
-
-  useEffect(() => {
-    console.log({props});
-  }, []);
+const Mappage = () => {
+  const [selectPosition, setSelectPosition] = useState(null);
 
   return (
-    <MapContainer
-      center={position}
-      zoom={13}
-      scrollWheelZoom={false}
-    >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://api.maptiler.com/maps/basic/256/{z}/{x}/{y}.png?key=PcY9RDLa27ZGAQ2dna7O"
-      />
-      {selectPosition && (
-        <Marker position={locationSelection} icon={icon}>
-          <Popup>
-          {selectPosition.address.house_number && selectPosition.address.road && (
-    <>
-      {selectPosition.address.house_number} {selectPosition.address.road}
-      <br />
-    </>
-  )}
-  {(selectPosition.address.city || selectPosition.address.state || selectPosition.address.postcode) && (
-    <>
-      {selectPosition.address.city} {selectPosition.address.state} {selectPosition.address.postcode}
-      <br />
-    </>
-  )}
-  {selectPosition.address.country}
-          </Popup>
-        </Marker>
-      )}
-      <ResetCenterView selectPosition={selectPosition} />
-    </MapContainer>
-  );
+<div className='col-md-8 '>
+<Map selectPosition={selectPosition} />
+<section className='home-search'>
+    <div className='container'>
+      <SmallSearchBar selectPosition={selectPosition} setSelectPosition={setSelectPosition} />
+    </div>
+  </section>
+</div>  )
 }
+
+export default Mappage
+
+
