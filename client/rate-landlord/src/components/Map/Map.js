2564d97ae512fb010bb789c076d8adf95e2dcbf3
Sheida Rashidi <84582226+SheidaRa@users.noreply.github.com>
2023-12-07 17:53:00 +0100

    Add files via upload

--- client/rate-landlord/src/components/Map/Map.js
+++ client/rate-landlord/src/components/Map/Map.js
@@ -1,27 +1,76 @@
-import Map from '../components/Map/Map';
-import { useState } from "react";
-import SmallSearchBar from '../components/SmallSearchBar/SmallSearchBar'
+import React, { useEffect } from "react";
+import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+import "./Map.css";
 
+const icon = L.icon({
+  iconUrl: "/pin.svg",
+  iconSize: [38, 38], 
+});
 
-import "../components/Searchbar/SearchBar.css"
-import "../components/Searchbar/SearchResultsList.css"
-import "../components/Searchbar/SearchResult.css"
+const position = [44.95, -93.09];
 
+function ResetCenterView(props) {
+  const { selectPosition } = props;
+  const map = useMap();
 
-const Mappage = () => {
-  const [selectPosition, setSelectPosition] = useState(null);
+  useEffect(() => {
+    if (selectPosition) {
+      map.setView(
+        L.latLng(selectPosition?.geometry.coordinates[1], selectPosition?.geometry.coordinates[0]),
+        map.getZoom(),
+        {
+          animate: true
+        }
+      )
+    }
+  }, [selectPosition]);
 
-  return (
-<div className='col-md-8 '>
-<Map selectPosition={selectPosition} />
-<section className='home-search'>
-    <div className='container'>
-      <SmallSearchBar selectPosition={selectPosition} setSelectPosition={setSelectPosition} />
-    </div>
-  </section>
-</div>  )
+  return null;
 }
 
-export default Mappage
+export default function Maps(props) {
 
+  const { selectPosition } = props;
+  const locationSelection = selectPosition
+    ? [selectPosition.geometry.coordinates[1], selectPosition.geometry.coordinates[0]]
+    : null;
 
+  useEffect(() => {
+    console.log({props});
+  }, []);
+
+  return (
+    <MapContainer
+      center={position}
+      zoom={13}
+      scrollWheelZoom={false}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://api.maptiler.com/maps/basic/256/{z}/{x}/{y}.png?key=PcY9RDLa27ZGAQ2dna7O"
+      />
+      {selectPosition && (
+        <Marker position={locationSelection} icon={icon}>
+          <Popup>
+          {selectPosition.addresstags.housenumber && selectPosition.addresstags.street && (
+    <>
+      {selectPosition.addresstags.housenumber} {selectPosition.addresstags.street}
+      <br />
+    </>
+  )}
+  {(selectPosition.addresstags.city || selectPosition.addresstags.state || selectPosition.addresstags.postcode) && (
+    <>
+      {selectPosition.addresstags.city} {selectPosition.addresstags.state} {selectPosition.addresstags.postcode}
+      <br />
+    </>
+  )}
+  {selectPosition.country_code.toUpperCase()}
+          </Popup>
+        </Marker>
+      )}
+      <ResetCenterView selectPosition={selectPosition} />
+    </MapContainer>
+  );
+}
