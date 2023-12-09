16e2c7505f116771278ad7019aff92390fd235e3
Sheida Rashidi <Srashidi@macalester.edu>
2023-12-09 16:40:35 -0600

    profiles updated

--- client/rate-landlord/src/pages/UserProfile.js
+++ client/rate-landlord/src/pages/UserProfile.js
@@ -1,54 +1,56 @@
 import React from 'react';
-import { MDBCol, MDBContainer, MDBRow, MDBCard, MDBCardText, MDBCardBody, MDBCardImage, MDBBtn, MDBTypography } from 'mdb-react-ui-kit';
-import './CSS/LandlordProfile.css'
+import './CSS/UserProfile.css';
 
 
-export default function UserProfile() {
+const LandlordProfile = () => {
+  const profile = {
+    name: 'John Doe',
+    profilePic: 'https://via.placeholder.com/150',
+    properties: [
+      { name: 'Property 1', image: 'https://via.placeholder.com/150' },
+      { name: 'Property 2', image: 'https://via.placeholder.com/150' },
+      
+    ],
+    aboutMe: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit...',
+    numberOfProperties: 5,
+    numberOfReviews: 10,
+    averageRating: 4.5,
+  };
+
   return (
-      <MDBContainer className="py-5 h-100">
-        <MDBRow className="justify-content-center align-items-center h-100">
-          <MDBCol lg="9" xl="7">
-            <MDBCard>
-              <div className="rounded-top text-white d-flex flex-row" style={{ backgroundColor: '#000', height: '200px' }}>
-                <div className="ms-4 mt-5 d-flex flex-column" style={{ width: '150px' }}>
-                  <MDBCardImage src="/"
-                    alt="Generic placeholder image" className="mt-4 mb-2 img-thumbnail" fluid style={{ width: '150px', zIndex: '1' }} />
-                  <MDBBtn outline color="dark" style={{height: '36px', overflow: 'visible'}}>
-                    Edit profile
-                  </MDBBtn>
-                </div>
-                <div className="ms-3" style={{ marginTop: '130px' }}>
-                  <MDBTypography tag="h5">John doe</MDBTypography>
-                  <MDBCardText>New York</MDBCardText>
-                </div>
-              </div>
-              <div className="p-4 text-black" style={{ backgroundColor: '#f8f9fa' }}>
-                <div className="d-flex justify-content-end text-center py-1">
-                  <div>
-                    <MDBCardText className="mb-1 h5">253</MDBCardText>
-                    <MDBCardText className="small text-muted mb-0">Reviews</MDBCardText>
+    <div className="container profile-container">
+    <div className="profile-info">
+      <div className="row mt-4">
+        <div className="col-md-4 offset-md-1">
+          <img src={profile.profilePic} alt="Profile" className="img-fluid" />
+          <div className="mt-3">
+          <h1>{profile.name}'s Profile</h1>
+          </div>
+          </div>
+        <div className="col-md-6">
+            <h5>Number of Reviews: {profile.numberOfReviews}</h5>
+        </div>
+      </div>
+    </div>
+      <div className='row'>
+        <div className="col-md-10 offset-md-1">
+          <h2>Properties reviewd</h2>
+          <div className="row">
+            {profile.properties.map((property, index) => (
+              <div key={index} className="col-md-4 mb-3">
+                <div className="profile-card">
+                  <img src={property.image} className="card-img-top" alt={`Property ${index + 1}`} />
+                  <div className="card-body">
+                    <h5 className="card-title">{property.name}</h5>
                   </div>
                 </div>
               </div>
-              <MDBCardBody className="text-black p-4">
-                <div className="d-flex justify-content-between align-items-center mb-4">
-                  <MDBCardText className="lead fw-normal mb-0">My Reviews</MDBCardText>
-                  <MDBCardText className="mb-0"><a href="#!" className="text-muted">Show all</a></MDBCardText>
-                </div>
-                <MDBRow>
-                  <MDBCol className="mb-2">
-                    <MDBCardImage src="https://mdbcdn.b-cdn.net/img/Photos/Lightbox/Original/img%20(112).webp"
-                      alt="image 1" className="w-100 rounded-3" />
-                  </MDBCol>
-                  <MDBCol className="mb-2">
-                    <MDBCardImage src="/"
-                      alt="image 1" className="w-100 rounded-3" />
-                  </MDBCol>
-                </MDBRow>
-              </MDBCardBody>
-            </MDBCard>
-          </MDBCol>
-        </MDBRow>
-      </MDBContainer>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
   );
-}
+};
+
+export default LandlordProfile;
