4d26d153c39cca2d833deb465709a1a6a05f6b2f
Sheida Rashidi <Srashidi@macalester.edu>
2023-12-18 01:44:09 -0600

    updated about and resource

--- client/rate-landlord/src/pages/About.js
+++ client/rate-landlord/src/pages/About.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import './CSS/About.css'
+import React from "react";
+import "./CSS/About.css";
 
 const About = () => {
   return (
@@ -7,35 +7,70 @@ const About = () => {
       <section className="about-us">
         <h2>About Us</h2>
         <p>
-          Rate the Landlord was created as a tool for tenants to stay informed about housing the same way we stay informed about every other business, through crowd-sourced reviews.
+          Rate Your Landlord was created as a tool for tenants to stay informed
+          about housing the same way we stay informed about every other
+          business, through crowd-sourced reviews.
         </p>
         <p>
-          We know that tenants are often in the dark when it comes to renting with a new landlord. This conflicts with the standards we hold for every other business and service where reviews allow the consumer to make an informed decision based on reports of quality and conduct.
+          We know that tenants are often in the dark when it comes to renting
+          with a new landlord. This conflicts with the standards we hold for
+          every other business and service where reviews allow the consumer to
+          make an informed decision based on reports of quality and conduct.
         </p>
         <p>
-          Something as important as housing shouldn't be an exception. Reviewing landlords alongside other businesses will make for a more transparent marketplace. By sharing rental experiences, tenants can help others avoid situations of negligence or mistreatment and find landlords who will uphold best practices and adhere to their local legislation.
+          Something as important as housing shouldn't be an exception. Reviewing
+          landlords alongside other businesses will make for a more transparent
+          marketplace. By sharing rental experiences, tenants can help others
+          avoid situations of negligence or mistreatment and find landlords who
+          will uphold best practices and adhere to their local legislation.
         </p>
         <p>
-          Share your experiences, read the reviews, and help us keep one another safe, informed, and empowered.
+          Share your experiences, read the reviews, and help us keep one another
+          safe, informed, and empowered.
         </p>
       </section>
 
       <section className="faq">
         <h2>Frequently Asked Questions</h2>
         <ul>
-          <li>Why are the reviews anonymous?</li>
-          <li>How is reviewing landlords fair if you can't review tenants?</li>
-          <li>What should I do if I can't review my current landlord but still need help?</li>
-          <li>Why does RTL include landlord names?</li>
+          <li>
+            <strong>Why are the reviews anonymous?</strong>
+            <p>
+              Reviews are kept anonymous to ensure honest feedback without fear
+              of repercussion. It encourages transparency and helps in providing
+              genuine experiences.
+            </p>
+          </li>
+          <li>
+            <strong>
+              How is reviewing landlords fair if you can't review tenants?
+            </strong>
+            <p>
+              Landlord reviews aim to create accountability and transparency in
+              the renting process. However, tenant privacy is equally important,
+              and their information is protected.
+            </p>
+          </li>
+          <li>
+            <strong>Why does we include landlord names?</strong>
+            <p>
+              Including landlord names helps in fostering accountability and
+              clarity in reviews. It ensures accurate feedback and aids in
+              creating a reliable platform for both landlords and tenants.
+            </p>
+          </li>
         </ul>
       </section>
 
       <section className="privacy-policy">
         <h2>Privacy Policy</h2>
         <p>
-          At Rate the Landlord Inc., we are committed to protecting the privacy of those who submit reviews. All submissions made to our website are anonymous. We do not collect any personal data or information that could be used to identify you, such as your name, email address, or phone number.
+          At Rate Your Landlord we are committed to protecting the privacy
+          of those who submit reviews. All submissions made to our website are
+          anonymous. We do not collect any personal data or information that
+          could be used to identify you, such as your name, email address, or
+          phone number.
         </p>
-        {/* ... (rest of the Privacy Policy content) */}
       </section>
 
       {/* Other sections */}
--- client/rate-landlord/src/pages/Resources.js
+++ client/rate-landlord/src/pages/Resources.js
@@ -1,30 +1,35 @@
-import React from 'react';
-import { Container, Row, Col, Card, Form } from 'react-bootstrap';
+import React from "react";
+import "./CSS/Resources.css";
+
+import { Container, Row, Col, Card, Form } from "react-bootstrap";
 
 const ResourcePage = () => {
-  // Sample data 
+  // Sample data
   const resources = [
     {
-      name: 'MASSACHUSETTS UNION OF PUBLIC HOUSING TENANTS',
-      location: 'DORCHESTER, MASSACHUSETTS, US',
-      address: '784 Washington St, Suite 504',
-      phoneNumber: '617 825 9750',
+      name: "MASSACHUSETTS UNION OF PUBLIC HOUSING TENANTS",
+      location: "DORCHESTER, MASSACHUSETTS, US",
+      address: "784 Washington St, Suite 504",
+      phoneNumber: "617 825 9750",
       description:
-        'Mass Union is a nonprofit run by tenants for tenants. Our mission is to preserve and improve public housing across the state. We are union of over 60 Local Tenant Organizations and we are growing fast.',
+        "Mass Union is a nonprofit run by tenants for tenants. Our mission is to preserve and improve public housing across the state. We are union of over 60 Local Tenant Organizations and we are growing fast.",
     },
   ];
 
   return (
-    <Container className="mt-4">
+    <Container className="resource-page mt-4">
       <h2 className="mb-4">Resources</h2>
       <p>Need support? Consider joining your local tenant union!</p>
       <p>
-        Tenant unions and advocacy groups play an important role in empowering renters by providing support, information, and resources to help them understand their rights and responsibilities.
+        Tenant unions and advocacy groups play an important role in empowering
+        renters by providing support, information, and resources to help them
+        understand their rights and responsibilities.
       </p>
       <p>
-        To join a tenant union, begin by searching for local organizations in your state/province/region or territory.
+        To join a tenant union, begin by searching for local organizations in
+        your state/province/region or territory.
       </p>
-      
+
       {/* Filters / Search Section */}
       <Row className="mb-4">
         <Col md={6}>
@@ -37,7 +42,7 @@ const ResourcePage = () => {
           </Form.Control>
         </Col>
       </Row>
-      
+
       {/* Displaying Resources */}
       <Row>
         {resources.map((resource, index) => (
@@ -45,10 +50,14 @@ const ResourcePage = () => {
             <Card>
               <Card.Body>
                 <Card.Title>{resource.name}</Card.Title>
-                <Card.Subtitle className="mb-2 text-muted">{resource.location}</Card.Subtitle>
+                <Card.Subtitle className="mb-2 text-muted">
+                  {resource.location}
+                </Card.Subtitle>
                 <Card.Text>
-                  <strong>Address:</strong> {resource.address}<br />
-                  <strong>Phone Number:</strong> {resource.phoneNumber}<br />
+                  <strong>Address:</strong> {resource.address}
+                  <br />
+                  <strong>Phone Number:</strong> {resource.phoneNumber}
+                  <br />
                   {resource.description}
                 </Card.Text>
               </Card.Body>
